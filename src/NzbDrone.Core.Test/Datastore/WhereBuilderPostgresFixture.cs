Radarr/Radarr

Set up tests on postgres

src/NzbDrone.Core.Test/Datastore/WhereBuilderPostgresFixture.cs
@@ -1,0 +1,206 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.Datastore
+{
+    [TestFixture]
+    public class WhereBuilderPostgresFixture : CoreTest
+    {
+        private WhereBuilderPostgres _subject;
+
+        [OneTimeSetUp]
+        public void MapTables()
+        {
+            // Generate table mapping
+            Mocker.Resolve<DbFactory>();
+        }
+
+        private WhereBuilderPostgres Where(Expression<Func<Movie, bool>> filter)
+        {
+            return new WhereBuilderPostgres(filter, true, 0);
+        }
+
+        [Test]
+        public void postgres_where_equal_const()
+        {
+            _subject = Where(x => x.Id == 10);
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"Id\" = @Clause1_P1)");
+            _subject.Parameters.Get<int>("Clause1_P1").Should().Be(10);
+        }
+
+        [Test]
+        public void postgres_where_equal_variable()
+        {
+            var id = 10;
+            _subject = Where(x => x.Id == id);
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"Id\" = @Clause1_P1)");
+            _subject.Parameters.Get<int>("Clause1_P1").Should().Be(id);
+        }
+
+        [Test]
+        public void postgres_where_equal_property()
+        {
+            var movie = new Movie { Id = 10 };
+            _subject = Where(x => x.Id == movie.Id);
+
+            _subject.Parameters.ParameterNames.Should().HaveCount(1);
+            _subject.ToString().Should().Be($"(\"Movies\".\"Id\" = @Clause1_P1)");
+            _subject.Parameters.Get<int>("Clause1_P1").Should().Be(movie.Id);
+        }
+
+        [Test]
+        public void postgres_where_equal_joined_property()
+        {
+            _subject = Where(x => x.Profile.Id == 1);
+
+            _subject.Parameters.ParameterNames.Should().HaveCount(1);
+            _subject.ToString().Should().Be($"(\"Profiles\".\"Id\" = @Clause1_P1)");
+            _subject.Parameters.Get<int>("Clause1_P1").Should().Be(1);
+        }
+
+        [Test]
+        public void postgres_where_throws_without_concrete_condition_if_requiresConcreteCondition()
+        {
+            Expression<Func<Movie, Movie, bool>> filter = (x, y) => x.Id == y.Id;
+            _subject = new WhereBuilderPostgres(filter, true, 0);
+            Assert.Throws<InvalidOperationException>(() => _subject.ToString());
+        }
+
+        [Test]
+        public void postgres_where_allows_abstract_condition_if_not_requiresConcreteCondition()
+        {
+            Expression<Func<Movie, Movie, bool>> filter = (x, y) => x.Id == y.Id;
+            _subject = new WhereBuilderPostgres(filter, false, 0);
+            _subject.ToString().Should().Be($"(\"Movies\".\"Id\" = \"Movies\".\"Id\")");
+        }
+
+        [Test]
+        public void postgres_where_string_is_null()
+        {
+            _subject = Where(x => x.CleanTitle == null);
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"CleanTitle\" IS NULL)");
+        }
+
+        [Test]
+        public void postgres_where_string_is_null_value()
+        {
+            string cleanTitle = null;
+            _subject = Where(x => x.CleanTitle == cleanTitle);
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"CleanTitle\" IS NULL)");
+        }
+
+        [Test]
+        public void postgres_where_equal_null_property()
+        {
+            var movie = new Movie { CleanTitle = null };
+            _subject = Where(x => x.CleanTitle == movie.CleanTitle);
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"CleanTitle\" IS NULL)");
+        }
+
+        [Test]
+        public void postgres_where_column_contains_string()
+        {
+            var test = "small";
+            _subject = Where(x => x.CleanTitle.Contains(test));
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"CleanTitle\" ILIKE '%' || @Clause1_P1 || '%')");
+            _subject.Parameters.Get<string>("Clause1_P1").Should().Be(test);
+        }
+
+        [Test]
+        public void postgres_where_string_contains_column()
+        {
+            var test = "small";
+            _subject = Where(x => test.Contains(x.CleanTitle));
+
+            _subject.ToString().Should().Be($"(@Clause1_P1 ILIKE '%' || \"Movies\".\"CleanTitle\" || '%')");
+            _subject.Parameters.Get<string>("Clause1_P1").Should().Be(test);
+        }
+
+        [Test]
+        public void postgres_where_column_starts_with_string()
+        {
+            var test = "small";
+            _subject = Where(x => x.CleanTitle.StartsWith(test));
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"CleanTitle\" ILIKE @Clause1_P1 || '%')");
+            _subject.Parameters.Get<string>("Clause1_P1").Should().Be(test);
+        }
+
+        [Test]
+        public void postgres_where_column_ends_with_string()
+        {
+            var test = "small";
+            _subject = Where(x => x.CleanTitle.EndsWith(test));
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"CleanTitle\" ILIKE '%' || @Clause1_P1)");
+            _subject.Parameters.Get<string>("Clause1_P1").Should().Be(test);
+        }
+
+        [Test]
+        public void postgres_where_in_list()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            _subject = Where(x => list.Contains(x.Id));
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"Id\" = ANY (('{{1, 2, 3}}')))");
+        }
+
+        [Test]
+        public void postgres_where_in_list_2()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            _subject = Where(x => x.CleanTitle == "test" && list.Contains(x.Id));
+
+            _subject.ToString().Should().Be($"((\"Movies\".\"CleanTitle\" = @Clause1_P1) AND (\"Movies\".\"Id\" = ANY (('{{1, 2, 3}}'))))");
+        }
+
+        [Test]
+        public void postgres_where_in_string_list()
+        {
+            var list = new List<string> { "first", "second", "third" };
+
+            _subject = Where(x => list.Contains(x.CleanTitle));
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"CleanTitle\" = ANY (@Clause1_P1))");
+        }
+
+        [Test]
+        public void enum_as_int()
+        {
+            _subject = Where(x => x.Status == MovieStatusType.Announced);
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"Status\" = @Clause1_P1)");
+        }
+
+        [Test]
+        public void enum_in_list()
+        {
+            var allowed = new List<MovieStatusType> { MovieStatusType.Announced, MovieStatusType.InCinemas };
+            _subject = Where(x => allowed.Contains(x.Status));
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"Status\" = ANY (@Clause1_P1))");
+        }
+
+        [Test]
+        public void enum_in_array()
+        {
+            var allowed = new MovieStatusType[] { MovieStatusType.Announced, MovieStatusType.InCinemas };
+            _subject = Where(x => allowed.Contains(x.Status));
+
+            _subject.ToString().Should().Be($"(\"Movies\".\"Status\" = ANY (@Clause1_P1))");
+        }
+    }
+}

src/NzbDrone.Core/Datastore/PostgresOptions.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Configuration;
+
 namespace NzbDrone.Core.Datastore
 {
     public class PostgresOptions
@@ -8,5 +10,17 @@ namespace NzbDrone.Core.Datastore
         public string Password { get; set; }
         public string MainDb { get; set; }
         public string LogDb { get; set; }
+
+        public static PostgresOptions GetOptions()
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables("Radarr__")
+                .Build();
+
+            var postgresOptions = new PostgresOptions();
+            config.GetSection("Postgres").Bind(postgresOptions);
+
+            return postgresOptions;
+        }
     }
 }

src/NzbDrone.Test.Common/NzbDroneRunner.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -9,7 +10,9 @@ using NUnit.Framework;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Processes;
+using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Datastore;
 using RestSharp;
 
 namespace NzbDrone.Test.Common
@@ -22,13 +25,15 @@ namespace NzbDrone.Test.Common
 
         public string AppData { get; private set; }
         public string ApiKey { get; private set; }
+        public PostgresOptions PostgresOptions { get; private set; }
         public int Port { get; private set; }
 
-        public NzbDroneRunner(Logger logger, int port = 7878)
+        public NzbDroneRunner(Logger logger, PostgresOptions postgresOptions, int port = 7878)
         {
             _processProvider = new ProcessProvider(logger);
             _restClient = new RestClient($"http://localhost:{port}/api/v3");
 
+            PostgresOptions = postgresOptions;
             Port = port;
         }
 
@@ -132,9 +137,23 @@ namespace NzbDrone.Test.Common
 
         private void Start(string outputRadarrConsoleExe)
         {
+            StringDictionary envVars = new ();
+            if (PostgresOptions?.Host != null)
+            {
+                envVars.Add("Radarr__Postgres__Host", PostgresOptions.Host);
+                envVars.Add("Radarr__Postgres__Port", PostgresOptions.Port.ToString());
+                envVars.Add("Radarr__Postgres__User", PostgresOptions.User);
+                envVars.Add("Radarr__Postgres__Password", PostgresOptions.Password);
+                envVars.Add("Radarr__Postgres__MainDb", PostgresOptions.MainDb);
+                envVars.Add("Radarr__Postgres__LogDb", PostgresOptions.LogDb);
+
+                TestContext.Progress.WriteLine("Using env vars:\n{0}", envVars.ToJson());
+            }
+
             TestContext.Progress.WriteLine("Starting instance from {0} on port {1}", outputRadarrConsoleExe, Port);
+
             var args = "-nobrowser -nosingleinstancecheck -data=\"" + AppData + "\"";
-            _nzbDroneProcess = _processProvider.Start(outputRadarrConsoleExe, args, null, OnOutputDataReceived, OnOutputDataReceived);
+            _nzbDroneProcess = _processProvider.Start(outputRadarrConsoleExe, args, envVars, OnOutputDataReceived, OnOutputDataReceived);
         }
 
         private void OnOutputDataReceived(string data)

CrackerCat/Readarr

src/NzbDrone.Core.Test/Datastore/WhereBuilderPostgresFixture.cs
@@ -1,0 +1,211 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Core.Books;
+using NzbDrone.Core.Datastore;
+using NzbDrone.Core.Test.Framework;
+
+namespace NzbDrone.Core.Test.Datastore
+{
+    [TestFixture]
+    public class WhereBuilderPostgresFixture : CoreTest
+    {
+        private WhereBuilderPostgres _subject;
+
+        [OneTimeSetUp]
+        public void MapTables()
+        {
+            // Generate table mapping
+            Mocker.Resolve<DbFactory>();
+        }
+
+        private WhereBuilderPostgres Where(Expression<Func<Author, bool>> filter)
+        {
+            return new WhereBuilderPostgres(filter, true, 0);
+        }
+
+        private WhereBuilderPostgres WhereMetadata(Expression<Func<AuthorMetadata, bool>> filter)
+        {
+            return new WhereBuilderPostgres(filter, true, 0);
+        }
+
+        [Test]
+        public void postgres_where_equal_const()
+        {
+            _subject = Where(x => x.Id == 10);
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"Id\" = @Clause1_P1)");
+            _subject.Parameters.Get<int>("Clause1_P1").Should().Be(10);
+        }
+
+        [Test]
+        public void postgres_where_equal_variable()
+        {
+            var id = 10;
+            _subject = Where(x => x.Id == id);
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"Id\" = @Clause1_P1)");
+            _subject.Parameters.Get<int>("Clause1_P1").Should().Be(id);
+        }
+
+        [Test]
+        public void postgres_where_equal_property()
+        {
+            var author = new Author { Id = 10 };
+            _subject = Where(x => x.Id == author.Id);
+
+            _subject.Parameters.ParameterNames.Should().HaveCount(1);
+            _subject.ToString().Should().Be($"(\"Authors\".\"Id\" = @Clause1_P1)");
+            _subject.Parameters.Get<int>("Clause1_P1").Should().Be(author.Id);
+        }
+
+        [Test]
+        public void postgres_where_equal_joined_property()
+        {
+            _subject = Where(x => x.QualityProfile.Value.Id == 1);
+
+            _subject.Parameters.ParameterNames.Should().HaveCount(1);
+            _subject.ToString().Should().Be($"(\"QualityProfiles\".\"Id\" = @Clause1_P1)");
+            _subject.Parameters.Get<int>("Clause1_P1").Should().Be(1);
+        }
+
+        [Test]
+        public void postgres_where_throws_without_concrete_condition_if_requiresConcreteCondition()
+        {
+            Expression<Func<Author, Author, bool>> filter = (x, y) => x.Id == y.Id;
+            _subject = new WhereBuilderPostgres(filter, true, 0);
+            Assert.Throws<InvalidOperationException>(() => _subject.ToString());
+        }
+
+        [Test]
+        public void postgres_where_allows_abstract_condition_if_not_requiresConcreteCondition()
+        {
+            Expression<Func<Author, Author, bool>> filter = (x, y) => x.Id == y.Id;
+            _subject = new WhereBuilderPostgres(filter, false, 0);
+            _subject.ToString().Should().Be($"(\"Authors\".\"Id\" = \"Authors\".\"Id\")");
+        }
+
+        [Test]
+        public void postgres_where_string_is_null()
+        {
+            _subject = Where(x => x.CleanName == null);
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"CleanName\" IS NULL)");
+        }
+
+        [Test]
+        public void postgres_where_string_is_null_value()
+        {
+            string cleanName = null;
+            _subject = Where(x => x.CleanName == cleanName);
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"CleanName\" IS NULL)");
+        }
+
+        [Test]
+        public void postgres_where_equal_null_property()
+        {
+            var author = new Author { CleanName = null };
+            _subject = Where(x => x.CleanName == author.CleanName);
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"CleanName\" IS NULL)");
+        }
+
+        [Test]
+        public void postgres_where_column_contains_string()
+        {
+            var test = "small";
+            _subject = Where(x => x.CleanName.Contains(test));
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"CleanName\" ILIKE '%' || @Clause1_P1 || '%')");
+            _subject.Parameters.Get<string>("Clause1_P1").Should().Be(test);
+        }
+
+        [Test]
+        public void postgres_where_string_contains_column()
+        {
+            var test = "small";
+            _subject = Where(x => test.Contains(x.CleanName));
+
+            _subject.ToString().Should().Be($"(@Clause1_P1 ILIKE '%' || \"Authors\".\"CleanName\" || '%')");
+            _subject.Parameters.Get<string>("Clause1_P1").Should().Be(test);
+        }
+
+        [Test]
+        public void postgres_where_column_starts_with_string()
+        {
+            var test = "small";
+            _subject = Where(x => x.CleanName.StartsWith(test));
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"CleanName\" ILIKE @Clause1_P1 || '%')");
+            _subject.Parameters.Get<string>("Clause1_P1").Should().Be(test);
+        }
+
+        [Test]
+        public void postgres_where_column_ends_with_string()
+        {
+            var test = "small";
+            _subject = Where(x => x.CleanName.EndsWith(test));
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"CleanName\" ILIKE '%' || @Clause1_P1)");
+            _subject.Parameters.Get<string>("Clause1_P1").Should().Be(test);
+        }
+
+        [Test]
+        public void postgres_where_in_list()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            _subject = Where(x => list.Contains(x.Id));
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"Id\" = ANY (('{{1, 2, 3}}')))");
+        }
+
+        [Test]
+        public void postgres_where_in_list_2()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            _subject = Where(x => x.CleanName == "test" && list.Contains(x.Id));
+
+            _subject.ToString().Should().Be($"((\"Authors\".\"CleanName\" = @Clause1_P1) AND (\"Authors\".\"Id\" = ANY (('{{1, 2, 3}}'))))");
+        }
+
+        [Test]
+        public void postgres_where_in_string_list()
+        {
+            var list = new List<string> { "first", "second", "third" };
+
+            _subject = Where(x => list.Contains(x.CleanName));
+
+            _subject.ToString().Should().Be($"(\"Authors\".\"CleanName\" = ANY (@Clause1_P1))");
+        }
+
+        [Test]
+        public void enum_as_int()
+        {
+            _subject = WhereMetadata(x => x.Status == AuthorStatusType.Continuing);
+
+            _subject.ToString().Should().Be($"(\"AuthorMetadata\".\"Status\" = @Clause1_P1)");
+        }
+
+        [Test]
+        public void enum_in_list()
+        {
+            var allowed = new List<AuthorStatusType> { AuthorStatusType.Continuing, AuthorStatusType.Ended };
+            _subject = WhereMetadata(x => allowed.Contains(x.Status));
+
+            _subject.ToString().Should().Be($"(\"AuthorMetadata\".\"Status\" = ANY (@Clause1_P1))");
+        }
+
+        [Test]
+        public void enum_in_array()
+        {
+            var allowed = new AuthorStatusType[] { AuthorStatusType.Continuing, AuthorStatusType.Ended };
+            _subject = WhereMetadata(x => allowed.Contains(x.Status));
+
+            _subject.ToString().Should().Be($"(\"AuthorMetadata\".\"Status\" = ANY (@Clause1_P1))");
+        }
+    }
+}

src/NzbDrone.Core/Datastore/PostgresOptions.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Configuration;
+
 namespace NzbDrone.Core.Datastore
 {
     public class PostgresOptions
@@ -9,5 +11,17 @@ namespace NzbDrone.Core.Datastore
         public string MainDb { get; set; }
         public string LogDb { get; set; }
         public string CacheDb { get; set; }
+
+        public static PostgresOptions GetOptions()
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables("Readarr__")
+                .Build();
+
+            var postgresOptions = new PostgresOptions();
+            config.GetSection("Postgres").Bind(postgresOptions);
+
+            return postgresOptions;
+        }
     }
 }

src/NzbDrone.Test.Common/NzbDroneRunner.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
@@ -9,7 +10,9 @@ using NUnit.Framework;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Processes;
+using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Datastore;
 using RestSharp;
 
 namespace NzbDrone.Test.Common
@@ -23,13 +26,15 @@ namespace NzbDrone.Test.Common
 
         public string AppData { get; private set; }
         public string ApiKey { get; private set; }
+        public PostgresOptions PostgresOptions { get; private set; }
         public int Port { get; private set; }
 
-        public NzbDroneRunner(Logger logger, int port = 8787)
+        public NzbDroneRunner(Logger logger, PostgresOptions postgresOptions, int port = 8787)
         {
             _processProvider = new ProcessProvider(logger);
             _restClient = new RestClient($"http://localhost:{port}/api/v1");
 
+            PostgresOptions = postgresOptions;
             Port = port;
         }
 
@@ -142,10 +147,24 @@ namespace NzbDrone.Test.Common
 
         private void Start(string outputNzbdroneConsoleExe)
         {
+            StringDictionary envVars = new ();
+            if (PostgresOptions?.Host != null)
+            {
+                envVars.Add("Readarr__Postgres__Host", PostgresOptions.Host);
+                envVars.Add("Readarr__Postgres__Port", PostgresOptions.Port.ToString());
+                envVars.Add("Readarr__Postgres__User", PostgresOptions.User);
+                envVars.Add("Readarr__Postgres__Password", PostgresOptions.Password);
+                envVars.Add("Readarr__Postgres__MainDb", PostgresOptions.MainDb);
+                envVars.Add("Readarr__Postgres__LogDb", PostgresOptions.LogDb);
+                envVars.Add("Readarr__Postgres__CacheDb", PostgresOptions.CacheDb);
+
+                TestContext.Progress.WriteLine("Using env vars:\n{0}", envVars.ToJson());
+            }
+
             TestContext.Progress.WriteLine("Starting instance from {0} on port {1}", outputNzbdroneConsoleExe, Port);
 
             var args = "-nobrowser -nosingleinstancecheck -data=\"" + AppData + "\"";
-            _nzbDroneProcess = _processProvider.Start(outputNzbdroneConsoleExe, args, null, OnOutputDataReceived, OnOutputDataReceived);
+            _nzbDroneProcess = _processProvider.Start(outputNzbdroneConsoleExe, args, envVars, OnOutputDataReceived, OnOutputDataReceived);
         }
 
         private void OnOutputDataReceived(string data)